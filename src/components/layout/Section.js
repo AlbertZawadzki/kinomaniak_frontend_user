4bf1718010a5df20ed7377f4b8970f00833933ac
albert <albert.zawadzki97@gmail.com>
2021-07-30 08:54:51 +0200

    Fixed sections responviseness

--- src/components/layout/Section.js
+++ src/components/layout/Section.js
@@ -10,6 +10,37 @@ class Section extends React.Component {
     speed: 500,
     slidesToShow: 6,
     slidesToScroll: 6,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 430,
+        settings: {
+          center: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   }
 
   render() {
